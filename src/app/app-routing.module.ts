15e1ffcf62cd9f806dc515c52aa974ccac383b95
Shantz Medley <tsm012@bucknell.edu>
2020-05-30 21:27:39 -0700

    initial angular commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { InterceptorService } from './interceptor.service';
 
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorService,
+      multi: true
+    }
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Auth0AngularDemo';
+export class AppComponent implements OnInit {
+  constructor(public auth: AuthService, private http: HttpClient) { }
+
+  title = 'AngularAuth0Demo';
+  weatherItems: any = [];
+
+  ngOnInit() {
+    this.http.get('http://localhost:5000/WeatherForecast').subscribe((reply) => {
+        this.weatherItems = reply;
+        console.log(this.weatherItems);
+    });
+  }
 }
